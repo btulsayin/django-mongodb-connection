d8a96fa32b5b83ff1b6a39dbd59691302c89a9cf
Betül Çil <btul.cil@gmail.com>
2018-06-22 22:44:53 +0300

    Update settings.py

--- mongodb_djongo/settings.py
+++ mongodb_djongo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your secret key...'
+SECRET_KEY = '*p1_7%@(ulhwlfw@q5!u3%jj$iuapyd97ja&j5b$m*#ct80kea'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
